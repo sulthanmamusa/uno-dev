bf50c5d6bfd6c1ddaeea5c32ea40cc3bf960096d
Viswas kg <viswas.k@provabmail.com>
2019-10-18 10:34:27 +0530

    added payments controller and included into merchant-api module

---
+++ src/merchant-api/controllers/payments.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Post, Param, Get } from '@nestjs/common';
+import { BaseController } from './base.controller';
+
+@Controller('api/payments')
+export class PaymentsController extends BaseController{
+    constructor(){
+        super()
+    }
+
+    @Get()
+    async findAll(): Promise<any>{
+        return 'fetch all payments!';
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: number): Promise<any>{
+        return 'fetch a single payment!';
+    }
+
+    @Post(':id/capture')
+    async capture(@Param('id') id: number): Promise<any>{
+        return 'payment captured successfully!';
+    }
+
+    @Post(':id/card')
+    async card(@Param('id') id: number): Promise<any>{
+        return {
+            "id": "card_6krZ6bcjoeqyV9",
+            "entity": "card",
+            "name": "Gaurav",
+            "last4": "3335",
+            "network": "Visa",
+            "type": "debit",
+            "issuer": "SBIN",
+            "international": false,
+            "emi": null
+        };
+    }
+
+}
--- src/merchant-api/merchant-api.module.ts
+++ src/merchant-api/merchant-api.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './controllers/orders.controller';
 import { CustomersController } from './controllers/customers.controller';
+import { PaymentsController } from './controllers/payments.controller';
 
 @Module({
   controllers: [
     CustomersController,
-    OrdersController
+    OrdersController,
+    PaymentsController
   ]
 })
 export class MerchantApiModule {}
