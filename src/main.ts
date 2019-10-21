870e994cc51bf154215dfa1a86e6e865710d4fdc
Viswas kg <viswas.k@provabmail.com>
2019-10-21 15:51:27 +0530

    modified main.ts to add the swagger api docs

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { ValidationPipe } from '@nestjs/common';
+import { MerchantPanelModule } from './merchant-panel/merchant-panel.module';
+import { MerchantApiModule } from './merchant-api/merchant-api.module';
 
 const PORT = process.env.PORT || 3000;
 
@@ -27,10 +29,16 @@ async function bootstrap() {
   .build();
 
   const document = SwaggerModule.createDocument(app, options, {include:[DatabaseModule]});
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('admin-api-docs', app, document);
 
   const document2 = SwaggerModule.createDocument(app, options, {include:[AuthModule]});
-  SwaggerModule.setup('api2', app, document2);
+  SwaggerModule.setup('auth-api-docs', app, document2);
+
+  const document3 = SwaggerModule.createDocument(app, options, {include:[MerchantPanelModule]});
+  SwaggerModule.setup('merchant-api-docs', app, document3);
+
+  const document4 = SwaggerModule.createDocument(app, options, {include:[MerchantApiModule]});
+  SwaggerModule.setup('pg-api-docs', app, document4);
 
   await app.listen(PORT);
 }
