a1c674217adbc9518ef38958f5df7439c1c1c1dc
Viswas kg <viswas.k@provabmail.com>
2019-10-21 15:07:57 +0530

    added @IsNotEmpty to all entities for class validator

--- src/database/entities/base.entity.ts
+++ src/database/entities/base.entity.ts
@@ -1,11 +1,8 @@
 import { PrimaryGeneratedColumn, Column  } from 'typeorm';
-import { IsDate, IsBoolean } from "class-validator";
+
 export class BaseEntity {
 	@PrimaryGeneratedColumn() id: number;
-	@IsDate()
 	@Column() createdat: Date;
-	@IsDate()
 	@Column() modifiedat: Date;
-	@IsBoolean()
 	@Column() status: boolean;
 }
